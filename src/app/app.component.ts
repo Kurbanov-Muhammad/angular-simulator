c7c62c63f033cdd53116b456deaf3c2f7b21e1e8
Kurbanov Muhammad <rvdhb@gmail.com>
2026-09-06 19:40:53 +0300

    Coded the block, used `@for` and two-way binding.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,36 +2,98 @@ import { Component } from '@angular/core';
 import './training';
 import { Color } from '../enums/Color';
 import './collection';
-import { NonNullableFormBuilder } from '@angular/forms';
+import { IService } from '../app/interfaces/IService';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   companySlogan: string = 'Насладись прогулкой в горах';
   continuationSlogan: string = 'с командой единомышленников';
+  hoveredImageId: number | null = null;
+  selectedLocation: string = '';
+  selectedDate: string = '';
+  participantsCount: string = '';
+  liveInputValue: string = '';
+  currentTime: string = '';
+  clickCount: number = 0;
+  showClock: boolean = true;
+  isLoading: boolean = true;
 
-  isPrimaryColor(color: Color): boolean {
+  services: IService[] = [
+    {
+      id: 1,
+      icon: './images/people_icon.svg',
+      title: 'Опытный гид',
+      description:
+        'Для современного мира базовый вектор развития предполагает независимые способы реализации соответствующих условий активизации.',
+    },
+    {
+      id: 2,
+      icon: './images/shield_icon.svg',
+      title: 'Безопасный поход',
+      description:
+        'Для современного мира базовый вектор развития предполагает независимые способы реализации соответствующих условий активизации',
+    },
+    {
+      id: 3,
+      icon: './images/tag_icon.svg',
+      title: 'Лояльные цены',
+      description:
+        'Для современного мира базовый вектор развития предполагает независимые способы реализации соответствующих условий активизации',
+    },
+  ];
+
+  private isPrimaryColor(color: Color): boolean {
     return color === Color.RED_COLOR || color === Color.BLUE_COLOR || color === Color.GREEN_COLOR;
   }
 
-  saveLastVisitDate(): void {
+  private saveLastVisitDate(): void {
     localStorage.setItem('lastVisit', new Date().toString());
   }
 
   constructor() {
     this.saveLastVisitDate();
     this.incrementVisitCount();
+    this.startClock();
+    setTimeout(() => {
+      this.isLoading = false;
+    }, 2000);
   }
 
-  incrementVisitCount(): void {
+  private incrementVisitCount(): void {
     const visitCount: string | null = localStorage.getItem('visitCount');
     const count: number = Number(visitCount) || 0;
     localStorage.setItem('visitCount', (count + 1).toString());
   }
+
+  isFormInvalid(): boolean {
+    return (
+      this.selectedLocation === '' || this.selectedDate === '' || this.participantsCount === ''
+    );
+  }
+
+  private startClock(): void {
+    setInterval(() => {
+      this.currentTime = new Date().toString();
+    }, 1000);
+  }
+
+  incrementClicks(): void {
+    this.clickCount++;
+  }
+
+  decrementClicks(): void {
+    if (this.clickCount > 0) {
+      this.clickCount--;
+    }
+  }
+
+  toggleView(): void {
+    this.showClock = !this.showClock;
+  }
 }
-
-
